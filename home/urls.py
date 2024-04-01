c5a97df8db06a786b39fd07d15e08ab32c7876ea
FelipeNunes <felipe.nunes.morgado@gmail.com>
2024-04-01 17:59:46 -0300

    Adição da tela de login
    Adicionamos html e css para a tela de login

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('inicio/', views.inicio, name = "inicio")
+    path('inicio/', views.inicio, name = "inicio") ,
+    path('login/', views.login, name = "login")
 ]
+

2624e04643f603b0770453b78e548872a8f4b9f7
LucasHolandaBarros <142419627+LucasHolandaBarros@users.noreply.github.com>
2024-04-04 16:40:17 -0300

    Criando banco de dados da personalização de usuário

--- home/models.py
+++ home/models.py
@@ -1,2 +1,9 @@
 from django.db import models
 
+class PersonUser(models.Model):
+    imagem = models.URLField()
+    descricao = models.CharField(max_length=500)
+
+    def __str__ (self) -> str:
+        return self.nome
+  

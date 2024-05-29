0ef3fedc721e9996792e812505b611b42582893b
LucasHolandaBarros <142419627+LucasHolandaBarros@users.noreply.github.com>
2024-05-29 08:16:58 -0300

    Adicionando testes independentes

--- cypress/e2e/feedback.cy.js
+++ cypress/e2e/feedback.cy.js
@@ -1,15 +1,28 @@
+Cypress.Commands.add('deleteAllUsers', () => {
+    cy.exec('python delete.py', { failOnNonZeroExit: true })
+});
+
 describe('test feedbacks', () => {
+    
     it('cenario1', () => {
         cy.visit('/');
+        cy.exec('python manage.py migrate')
+        cy.deleteAllUsers();
 
-        cy.get('#nome').type('Lucas')
-        cy.get('[type="password"]').type('12345')
+        cy.get('a').click()
+        cy.get('#nome').type("testfeedback")
+        cy.get('#email').type("teste@gmail.com")
+        cy.get('[type="password"]').type("12345")
         cy.get('.submitbtn').click()
-        cy.get('[type="Email"]').type('l@gmail.com')
-        cy.get('textarea').type('Testando')
+        cy.get('#nome').type("testfeedback")
+        cy.get('[type="password"]').type("12345")
+        cy.get('.submitbtn').click()
+        cy.get('[type="Email"]').type('teste@gmail.com')
+        cy.get('textarea').type('site muito legal e divertido')
         cy.get('.btn-custom').click()
         cy.get(':nth-child(4) > a').click()
         cy.get('[data-bs-target="#exampleModal"]').click()
+
         
     })
 

43dffafd3264c2ce0c7fe02b5b46b24874216305
LucasHolandaBarros <142419627+LucasHolandaBarros@users.noreply.github.com>
2024-05-29 16:22:34 -0300

    Independencializando todos os testes

--- cypress/e2e/projetopessoal.cy.js
+++ cypress/e2e/projetopessoal.cy.js
@@ -1,16 +1,32 @@
+Cypress.Commands.add('deleteAllUsers', () => {
+    cy.exec('python delete.py', { failOnNonZeroExit: true })
+});
+
 describe('test projetos pessoais', () => {
     it('cenario1', () => {
         cy.visit('/');
-
+        cy.exec('python manage.py migrate')
+        cy.deleteAllUsers();
+        
+        cy.get('a').click()
+        cy.wait(1500)
+        cy.get('#nome').type("User 1")
+        cy.wait(1500)
+        cy.get('#email').type("user@gmail.com")
+        cy.wait(1500)
+        cy.get('[type="password"]').type("12345")
+        cy.wait(1500)
+        cy.get('.submitbtn').click()
+        cy.wait(1500)
         cy.get('#nome').type('User 1')
         cy.wait(1500)
         cy.get('[type="password"]').type('12345')
         cy.wait(1500)
         cy.get('.submitbtn').click()
         cy.wait(1500)
-        cy.get(':nth-child(6) > a').click()
+        cy.get(':nth-child(7) > a').click()
         cy.wait(1500)
-        cy.get('#name').type('Testando')
+        cy.get('#name').type('Projeto Legal')
         cy.wait(1500)
         cy.get('#description').type('Descrição Legal')
         cy.wait(1500)
@@ -19,6 +35,8 @@ describe('test projetos pessoais', () => {
         cy.get('.btn').click()
         cy.wait(1500)
         cy.get(':nth-child(2) > a').click()
+        cy.wait(1500)
+        cy.get('.card-body > .btn').click()
         cy.wait(1500)
     })
 
